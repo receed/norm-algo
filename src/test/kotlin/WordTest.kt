c60471586c4665746f1a23035c069b532f9e57b3
Andrey Efremov <receed@localhost.localdomain>
2020-09-12 16:20:01 +0300

    Fix tests for word match

--- src/test/kotlin/WordTest.kt
+++ src/test/kotlin/WordTest.kt
@@ -3,13 +3,13 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 
 internal class WordTest {
-    val empty = Word("")
-    val a = Word("a")
-    val b = Word("b")
-    val abacaba = Word("abacaba")
-    val aaaa = Word("aaaa")
-    val caba = Word("caba")
-    val acaba = Word("acaba")
+    private val empty = Word("")
+    private val a = Word("a")
+    private val b = Word("b")
+    private val abacaba = Word("abacaba")
+    private val aaaa = Word("aaaa")
+    private val caba = Word("caba")
+    private val acaba = Word("acaba")
 
     @Test
     fun isEmpty() {
@@ -26,9 +26,12 @@ internal class WordTest {
 
     @Test
     fun firstMatch() {
-        assertEquals(empty.length(), 0)
-        assertEquals(abacaba.length(), 7)
-        assertEquals(aaaa.length(), 4)
+        assertEquals(empty.firstMatch(empty), 0)
+        assertEquals(aaaa.firstMatch(empty), 0)
+        assertEquals(b.firstMatch(b), 0)
+        assertEquals(abacaba.firstMatch(caba), 3)
+        assertEquals(aaaa.firstMatch(b), -1)
+        assertEquals(empty.firstMatch(a), -1)
     }
 
     @Test
