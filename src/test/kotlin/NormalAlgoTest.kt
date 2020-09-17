1b023cadd88023ea63bf8e696fe5195d74f2b5c0
receed <efremov-343@yandex.ru>
2020-09-17 21:31:06 +0300

    Add batch test

--- src/test/kotlin/NormalAlgoTest.kt
+++ src/test/kotlin/NormalAlgoTest.kt
@@ -1,9 +1,16 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.io.File
 
 internal class NormalAlgoTest {
     @Test
     fun batch() {
-
+        val inputFile = "data/test"
+        main(arrayOf(inputFile, "-b"))
+        File(inputFile).forEachLine {
+            val result = File("$it.out").readLines()
+            val expected = File("$it.a").readLines()
+            assertEquals(expected, result)
+        }
     }
 }
